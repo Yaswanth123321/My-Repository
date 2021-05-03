d04d6672dbef0dae48d2ea2c0fcb63a6fd6ff272
Yaswanth Gajula <75373183+Yaswanth123321@users.noreply.github.com>
2021-05-03 19:48:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LinkedListExample
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings  
+        var names = new LinkedList<string>();
+        names.AddLast("Abc");
+        names.AddLast("Agef");
+        names.AddLast("Pefdsr");
+        names.AddLast("Irddsc");
+
+        //insert new element before "Peter"  
+        LinkedListNode<String> node = names.Find("Agef");
+        names.AddBefore(node, "John");
+        names.AddAfter(node, "Lsfy");
+
+        // Iterate list element using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
