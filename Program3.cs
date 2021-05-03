823a3a2002a11b4b656202edd4bcf15d7e2fc506
Yaswanth Gajula <75373183+Yaswanth123321@users.noreply.github.com>
2021-05-03 19:59:19 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program3
+{
+    interface Iname1
+    {
+        public void enter();
+    }
+    interface Iname2
+    {
+        public void display();
+    }
+    class Myclass : Iname1,Iname2
+    {
+        int a;
+        public void enter()
+        {
+            Console.Write("Enter the number:");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+        public void display()
+        {
+            Console.WriteLine(a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass obj = new Myclass();
+            obj.enter();
+            obj.display();
+        }
+    }
+}
