3aebecb68e1faf463f207bdfada7a93da3524bc8
Yaswanth Gajula <75373183+Yaswanth123321@users.noreply.github.com>
2021-05-07 18:39:12 +0530

    Add files via upload

--- CoursesController.cs
+++ CoursesController.cs
@@ -9,38 +9,58 @@ namespace TrainingCompany.Controllers
 {
     public class CoursesController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<Course> AllCourses()
+        public static List<Course> courses = initcourse();
+        private static List<Course> initcourse()
+        {
+            var ret = new List<Course>();
+            ret.Add(new Course() { Id = 1, Title = "first" });
+            ret.Add(new Course() { Id = 2, Title = "second" });
+            ret.Add(new Course() { Id = 3, Title = "third" });
+            ret.Add(new Course() { Id = 4, Title = "fourth" });
+            ret.Add(new Course() { Id = 5, Title = "fifth" });
+            return ret;
+        }
+        public IEnumerable<Course> Get()
         {
             return courses;
         }
-        public static List<Course> courses = Initcourses();
-        private static List<Course> Initcourses()
+        public HttpResponseMessage Get(int id)
         {
-            var ret = new List<Course>() { new Course() { Id = 0, Title = "first" },
-                                           new Course() { Id = 1, Title = "second" },
-                                           new Course() { Id = 2, Title = "third" },
-                                           new Course() { Id = 3, Title = "fourth"},
-                                           new Course() { Id = 4, Title = "fifth"}};
-            return ret;
+            if (courses.Count > id)
+            {
+                var ret = (from c in courses where c.Id == id select c).FirstOrDefault();
+                return Request.CreateResponse<Course>(HttpStatusCode.OK, ret);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Error Not Found");
+            }
         }
-        public Course Get(int id)
-        {
-            var ret = (from c in courses
-                       where c.Id == id
-                       select c).FirstOrDefault();
-            return ret;
-
-        }
-        public void Post(Course c)
-        {
-            c.Id = courses.Count;
+        public HttpResponseMessage Post([FromBody] Course c)
+        { 
             courses.Add(c);
+            var msg = Request.CreateResponse(HttpStatusCode.Created);
+            msg.Headers.Location = new Uri(Request.RequestUri + "/" + (courses.Count).ToString());
+            return msg;
         }
-        public class Course
+        public HttpResponseMessage Put(int id,[FromBody]Course course1)
         {
-            public int Id { get; set; }
-            public string Title { get; set; }
+            var ret = (from c in courses where c.Id == id select c).FirstOrDefault();
+            ret.Title = course1.Title;
+            var msg1 = Request.CreateResponse(HttpStatusCode.Accepted);
+            msg1.Headers.Location = new Uri(Request.RequestUri.ToString());
+            return msg1;
         }
+        public HttpResponseMessage Delete(int id)
+        {
+            courses.RemoveAt(id);
+            var msg = Request.CreateResponse(HttpStatusCode.Gone);
+            return msg;
+        }
+    }
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
     }
 }
