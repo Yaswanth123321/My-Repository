d95090a3aeb0708d9edcffbb2650cfc0026e0b59
Yaswanth Gajula <75373183+Yaswanth123321@users.noreply.github.com>
2021-05-20 13:27:54 +0530

    Add files via upload

---
+++ ADOModel1/ADOModel1/AdoClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADOModel1
+{
+    class AdoClass
+    {
+        static void Main(string[] args)
+        {
+            AdoClass obj = new AdoClass();
+            obj.createtable();
+        }
+        public void createtable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("Data Source=.;Database=sample;Integrated Security=SSPI");
+                SqlCommand cmd = new SqlCommand("create table student(id int Primary Key NOT NULL,name Varchar(20) NOT NULL)", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Occured" + e);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
